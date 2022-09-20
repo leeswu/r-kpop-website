f4f9b0fa6335fc2d0be75ff04b8f8a1d469cf65c
swulsy <swu.lee02@gmail.com>
2022-09-20 16:27:25 -0500

    Adjusted schemas, testing api endpoint

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -11,12 +11,24 @@ export const getData = async (req, res)=> {
     }
 }
 
+
 export const getGroup = async (req, res)=> {
     try {
         const group = req.params.group;
         console.log(group);
         const data = await GroupData.findOne({Name: group});
         res.status(200).json(data);
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).json({message: err.message});
+    }
+}
+
+export const getComments = async (req, res)=> {
+    try {
+        const group = req.params.group;
+        const data = await GroupData.findOne({Name: group}, ['Num Comments']);
+        res.status(200).json(data['Num Comments']);
         console.log(data);
     } catch (err) {
         console.log(err.message);
--- server/routes/data.js
+++ server/routes/data.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {getData, getGroup} from '../controllers/data.js';
+import {getComments, getData, getGroup} from '../controllers/data.js';
 
 const router = express.Router();
 
 router.get('/', getData);
+router.get('/:group/comments', getComments);
 router.get('/:group', getGroup);
+
 
 export default router;
